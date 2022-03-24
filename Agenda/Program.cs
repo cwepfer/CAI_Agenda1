ddb302c4516b83e19d74fe31e3614e112efa1384
cwepfer <criswepfer@gmail.com>
2022-03-24 17:42:15 -0300

    cambios

---
+++ Agenda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+            int a;
+            a = 9;
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
